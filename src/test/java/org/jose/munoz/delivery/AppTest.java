b3f0445f49906fdde1bbbd72d1a001dc3f1ddcd4
José Muñoz Sierra <ing.jose.luis@hotmail.com>
2020-10-20 18:56:46 -0500

    initial setup project

---
+++ src/test/java/org/jose/munoz/delivery/AppTest.java
@@ -0,0 +1,20 @@
+package org.jose.munoz.delivery;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
